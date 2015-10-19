eecc12efe8e105e270b345ba33a8bd3a48cac82f
Victor Elias <victorgelias@gmail.com>
2015-10-19 10:38:38 -0200

    Exporting JS from 2048 as node.js module
    With some extra needed utilities

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@ var port = process.env.PORT || 8080;
 
 var staticdir = path.join(__dirname, '..', 'static');
 
-app.use('/js',    express.static(staticdir + '/js'));
-app.use('/style', express.static(staticdir + '/style'));
+app.use('/js',    express.static(path.join(staticdir, 'js')));
+app.use('/style', express.static(path.join(staticdir, 'style')));
 
 app.get('/', function(req, res) {
   console.log("Serving static index.html");
-  res.sendFile(staticdir + '/index.html');
+  res.sendFile(path.join(staticdir, 'index.html'));
 });
 
 app.listen(port);

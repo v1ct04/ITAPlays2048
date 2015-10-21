victorges/ITAPlays2048_old

Exporting JS from 2048 as node.js module
With some extra needed utilities

server/app.js
@@ -5,14 +5,14 @@ var path       = require('path');
 
 var port = process.env.PORT || 8080;
 
-var dir2048 = path.resolve(__dirname + '/../2048');
+var dir2048 = path.join(__dirname, '..', '2048');
 
-app.use('/js',    express.static(dir2048 + '/js'));
-app.use('/style', express.static(dir2048 + '/style'));
+app.use('/js',    express.static(path.join(dir2048, 'js')));
+app.use('/style', express.static(path.join(dir2048, 'style')));
 
 app.get('/', function(req, res) {
   console.log("Serving 2048's index.html");
-  res.sendFile(dir2048 + '/index.html');
+  res.sendFile(path.join(dir2048, 'index.html'));
 });
 
 app.listen(port);

v1ct04/ITAPlays2048

server/app.js
@@ -7,12 +7,12 @@ var port = process.env.PORT || 8080;
 
 var staticdir = path.join(__dirname, '..', 'static');
 
-app.use('/js',    express.static(staticdir + '/js'));
-app.use('/style', express.static(staticdir + '/style'));
+app.use('/js',    express.static(path.join(staticdir, 'js')));
+app.use('/style', express.static(path.join(staticdir, 'style')));
 
 app.get('/', function(req, res) {
   console.log("Serving static index.html");
-  res.sendFile(staticdir + '/index.html');
+  res.sendFile(path.join(staticdir, 'index.html'));
 });
 
 app.listen(port);